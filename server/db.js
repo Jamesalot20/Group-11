d45fc0ebc9d325981d9759f32f86cd5ee13e2974
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-06 19:41:36 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,18 @@
 
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb+srv://James:xxixQtTzY70iUVtP@ttdatabase.khylsrk.mongodb.net/test";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connect() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    const db = client.db("your_database_name");
+    return db;
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  }
+}
+
+module.exports = connect;
