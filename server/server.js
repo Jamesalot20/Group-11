f4fa7a5ae15e72c77f178b04454980ed9e6b0579
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-06 19:49:04 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,3 +5,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+// In your main server file, e.g., server.js
+const userRoutes = require('./userRoutes.js');
+const productRoutes = require('./productRoutes.js');
+
+// Use the routes as middleware
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
