506f2b676a2cef04081afdc5aa403afb63d89b78
tw0429 <117667211+tw0429@users.noreply.github.com>
2023-04-06 21:25:28 -0500

    Update users.js
    Search item and create product added.

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,5 +5,7 @@ const usersController = require('../controllers/usersController');
 router.post('/register', usersController.registerUser);
 router.post('/login', usersController.loginUser);
 router.get('/logout', usersController.logoutUser);
+router.post('/searchItems', usersController.searchItems);
+router.post('/createProduct', usersController.createProduct);
 
 module.exports = router;
