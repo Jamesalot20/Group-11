67d461aec0e797c1427dc68cc35243965eaa4ff5
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-06 19:56:34 -0500

    Create users.js

---
+++ server/routes/users.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+// Add your routes here
+router.post('/register', (req, res) => {
+  // Registration logic
+});
+
+router.post('/login', (req, res) => {
+  // Login logic
+});
+
+router.get('/logout', (req, res) => {
+  // Logout logic
+});
+
+module.exports = router;
