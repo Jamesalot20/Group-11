7e20c11eba37f277f83939c9008a0c0d4a617e8c
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-06 20:25:55 -0500

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,11 +1,58 @@
-exports.registerUser = (req, res) => {
-  // Registration logic
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({ message: 'User with this email already exists.' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: 'User successfully registered.' });
+
+  } catch (error) {
+    res.status(500).json({ message: 'Server error.' });
+  }
 };
 
-exports.loginUser = (req, res) => {
-  // Login logic
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password.' });
+    }
+
+    const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(200).json({ message: 'Login successful.', token });
+
+  } catch (error) {
+    res.status(500).json({ message: 'Server error.' });
+  }
 };
 
 exports.logoutUser = (req, res) => {
-  // Logout logic
+  // Since JWT tokens are stateless, you cannot invalidate the token on the server-side.
+  // To "log out" a user, simply remove the token from the client-side (e.g., delete it from local storage).
+  res.status(200).json({ message: 'Logout successful.' });
 };
