4f9683f9c841a94c1a5e069b79f33bb264d324d3
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-10 18:21:30 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,23 +1,35 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-// In your main server file, e.g., server.js
-const userRoutes = require('./routes/users');
-
-app.use('/api/users', userRoutes);
-
-// Use the routes as middleware
-app.use('/users', userRoutes);
-//app.use('/products', productRoutes);
-// In your main server file
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
+const dotenv = require('dotenv');
 
+// Import routes
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
+// Import error middleware
+const errorMiddleware = require('./middlewares/errorMiddleware');
+
+// Load environment variables
+dotenv.config();
+
+// Initialize Express
+const app = express();
+
+// Middleware
 app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+
+// Error handling middleware
+app.use(errorMiddleware.errorHandler);
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
