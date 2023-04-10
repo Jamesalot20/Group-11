bfe0b1c9620e54b620488960f0c6f56943523fc2
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-10 18:07:53 -0500

    Create authMiddleware.js

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+exports.authenticate = (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided, access denied.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid token.' });
+  }
+};
+
+exports.authorize = (roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Access denied.' });
+    }
+    next();
+  };
+};
