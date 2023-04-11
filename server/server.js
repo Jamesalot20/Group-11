38b29bd088010cf11715a12bbbf6df2cd2fa5931
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-10 22:56:27 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,3 +33,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+}).catch((error) => {
+  console.error('Failed to connect to the database:', error);
+});
