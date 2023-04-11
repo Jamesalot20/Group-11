6fd5cbe31331d48711dff54f7d01d5684040174a
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-11 17:29:32 -0500

    Update productsController.js

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -1,11 +1,12 @@
 const Product = require('../models/product');
+const Item = require('../models/Item');
 
 exports.getProducts = async (req, res) => {
   try {
-    const products = await Product.find();
-    res.status(200).json(products);
+    const items = await Item.find();
+    res.status(200).json({ items });
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while fetching products.' });
+    res.status(500).json({ message: 'Server error.' });
   }
 };
 
