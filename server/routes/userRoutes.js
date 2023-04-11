ea357d90c42878d2ab020c67e651b5a5d77353db
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-10 22:37:36 -0500

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,16 +11,4 @@ router.post('/searchItems', usersController.searchItems);
 router.post('/createProduct', usersController.createProduct);
 
 router.get('/protected', authMiddleware.authenticate, authMiddleware.authorize(['admin', 'seller']), usersController.protectedRoute);
-router.get('/userByEmail/:email', async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.params.email });
-    if (!user) {
-      res.status(404).json({ message: 'User not found' });
-    } else {
-      res.status(200).json(user);
-    }
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while fetching the user.' });
-  }
-});
 module.exports = router;
