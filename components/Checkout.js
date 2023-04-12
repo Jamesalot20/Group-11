dde68c5cf381502439e9fb72fe95fd569494ce4b
William-Emmich <117790219+William-Emmich@users.noreply.github.com>
2023-04-12 03:47:27 -0500

    Add files via upload
    Framework for checkout store and completion pages

---
+++ components/Checkout.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+
+export default class Checkout extends Component {
+    render() {
+        return (
+            <form>
+
+                <h1>Checkout</h1>
+                <h2>Total:</h2>
+
+                <div className="Products">
+
+                </div>
+
+                <div className="mb-3">
+                    <label>Address</label>
+                    <input
+                        type="address"
+                        className="form-control"
+                        placeholder="Enter address"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label>Card Number</label>
+                    <input
+                        type="card number"
+                        className="form-control"
+                        placeholder="Enter card number"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label>City</label>
+                    <input
+                        type="city"
+                        className="form-control"
+                        placeholder="Enter city"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label>State</label>
+                    <input
+                        type="state"
+                        className="form-control"
+                        placeholder="Enter state"
+                    />
+                </div>
+
+
+
+
+
+
+
+            </form>
+        )
+    }
+}
