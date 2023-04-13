08ccf366dd4563d23e510606c4143c900a195889
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-12 23:10:08 -0500

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,10 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 exports.authenticate = (req, res, next) => {
-  const token = req.headers && req.headers['x-auth-token'];
-  if (!token) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
     return res.status(401).json({ message: 'No token provided, access denied.' });
   }
+
+  const token = authHeader.split(' ')[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
