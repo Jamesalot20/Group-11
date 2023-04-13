a2bf87cfa5ac230166a6f05ef55e38cfd55553ed
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-13 12:33:58 -0500

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 const authMiddleware = require('../middlewares/authMiddleware');
-
+const productsController = require('../controllers/productsController');
 
 router.post('/register', usersController.registerUser);
 router.post('/login', usersController.loginUser);
