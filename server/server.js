8937310a443b8b3e931dda96938659c8abba0670
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-13 12:52:04 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: false }));
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
