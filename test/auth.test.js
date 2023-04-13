bbd7e776e9ab53b7b22ff671f00c022404807505
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-13 14:09:45 -0500

    Update auth.test.js

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,6 +1,6 @@
 const supertest = require('supertest');
 const chai = require('chai');
-const app = require('../server'); // Import your server or app instance
+const app = require('Group-11/server'); // Import your server or app instance
 const request = supertest(app);
 const { expect } = chai;
 
