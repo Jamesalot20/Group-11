386dc9fd51d92667fe576229de6693974124c469
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-13 13:57:35 -0500

    Update auth.test.js

--- test/auth.test.js
+++ test/auth.test.js
@@ -2,12 +2,6 @@ const supertest = require('supertest');
 const chai = require('chai');
 const app = require('../server'); // Import your server or app instance
 const request = supertest(app);
-const buyerLogin = await chai.request(app).post('/api/users/login').send({ email: test@example.com, password: password123 });
-const sellerLogin = await chai.request(app).post('/api/users/login').send({ email: sellertest@example.com, password: password123 });
-const adminLogin = await chai.request(app).post('/api/users/login').send({ email: jf1812@msstate.edu, password: Qwerre123 });
-const buyerProtectedRoute = await chai.request(app).get('/api/users/protected').set('Authorization', `Bearer ${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDM4NGFjNTczNGZmYjkyMjJlOGE5ODkiLCJyb2xlIjoiYnV5ZXIiLCJpYXQiOjE2ODE0MTIwNjcsImV4cCI6MTY4MTQxNTY2N30.5DQQZjTLPPSpoyiUpP6DUFxa2R4aKUsz2Rfsa6AIC4M}`);
-const sellerProtectedRoute = await chai.request(app).get('/api/users/protected').set('Authorization', `Bearer ${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDM4NGFlOTczNGZmYjkyMjJlOGE5OGMiLCJyb2xlIjoic2VsbGVyIiwiaWF0IjoxNjgxNDEyMDMxLCJleHAiOjE2ODE0MTU2MzF9.zgt2FrHn6madv1MxY-QW9grZ9gv-BJD5OTlV61AP_TA}`);
-const adminProtectedRoute = await chai.request(app).get('/api/users/protected').set('Authorization', `Bearer ${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDM3N2FiNTE0MWE2M2FlMTc3ZDc0YzAiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2ODE0MTIwOTUsImV4cCI6MTY4MTQxNTY5NX0.FSuTw-l06H5mYUq3l0yg29nkJuCbADEhPEdAb7qqcXE}`);
 const { expect } = chai;
 
 describe('Auth Tests', () => {
