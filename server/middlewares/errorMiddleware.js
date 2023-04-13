b40e08652017b6ccb7abed5754363a9bc510b5db
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-12 23:27:39 -0500

    Update errorMiddleware.js

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -1,5 +1,6 @@
 exports.errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  console.error(err);
   res.status(statusCode);
   res.json({
     message: err.message,
