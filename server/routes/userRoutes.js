aa2c2019311d2d878a22509443de62fb8f9ef22a
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-13 12:03:48 -0500

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,7 +7,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 router.post('/register', usersController.registerUser);
 router.post('/login', usersController.loginUser);
 router.get('/logout', usersController.logoutUser);
-router.post('/searchItems', usersController.searchItems);
+//router.post('/searchItems', usersController.searchItems);
 
 router.get('/protected', authMiddleware.authenticate, authMiddleware.authorize(['admin', 'seller']), usersController.protectedRoute);
 router.get('/userByEmail/:email', usersController.getUserByEmail);
