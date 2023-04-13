fff1725bfb3996d6f9394ce7db6a6a8888089beb
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-13 16:25:15 -0500

    Create productsController.js

---
+++ test/productsController.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server/server');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Product Tests', () => {
+  let sellerToken;
+
+  before(async () => {
+    // Login as a seller to obtain the authorization token
+    const sellerCredentials = {
+      email: 'seller@example.com',
+      password: 'sellerpassword'
+    };
+
+    const res = await chai.request(server)
+      .post('/api/users/login')
+      .send(sellerCredentials);
+      
+    sellerToken = res.body.token;
+  });
+
+  describe('Delete Product', () => {
+    it('should delete a product by a seller', (done) => {
+      const productId = 64383f539ee13ca9847d15e8; // Replace this with an actual product ID
+
+      chai.request(server)
+        .delete(`/api/products/${productId}`)
+        .set('Authorization', `Bearer ${sellerToken}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Product deleted successfully.');
+          done();
+        });
+    });
+  });
+});
