88c485681dea18979a286de29d57eb8ebb9ebecc
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-13 15:32:25 -0500

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,7 +7,7 @@ const productsController = require('../controllers/productsController');
 router.post('/register', usersController.registerUser);
 router.post('/login', usersController.loginUser);
 router.post('/createProduct', authMiddleware.authenticate, authMiddleware.authorize(['seller', 'admin']), productsController.createProduct);
-router.get('/logout', usersController.logoutUser);
+router.post('/logout', usersController.logoutUser);
 router.post('/searchItems', usersController.searchItems);
 
 router.get('/protected', authMiddleware.authenticate, authMiddleware.authorize(['admin', 'seller']), usersController.protectedRoute);
