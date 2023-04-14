19346b0e196952997053b0d811b1c76ac2e374cd
JDMCCOY-dev <85303175+JDMCCOY-dev@users.noreply.github.com>
2023-04-13 23:50:18 -0500

    Create viewAllProducts.js

---
+++ test/viewAllProducts.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const expect = chai.expect;
+const app = require('../server');
+const request = require('supertest');
+
+describe('Product Routes', () => {
+  describe('GET /api/products', () => {
+    it('should return all products for a buyer', (done) => {
+      request(app)
+        .get('/api/products')
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.be.an('array');
+          // Make sure all products are for buyers
+          const allProductsForBuyers = res.body.every(product => product.seller === 'admin');
+          expect(allProductsForBuyers).to.be.true;
+          done();
+        });
+    });
+  });
+});
