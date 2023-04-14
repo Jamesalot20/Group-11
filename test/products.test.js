35093607a913d7807c950d0717ddbc724dc028bc
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-13 19:18:45 -0500

    Update products.test.js

--- test/products.test.js
+++ test/products.test.js
@@ -5,36 +5,55 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Delete Product', () => {
-  let productId;
+// Move sellerToken declaration to the outer scope
+let sellerToken;
 
+describe('Product Tests', () => {
   before(async () => {
-    // Create a new product
-    const newProduct = {
-      name: 'Test Product',
-      description: 'Test Product Description',
-      price: 10,
-      category: 'Test Category',
-      seller: '64377ab5141a63ae177d74c0', // Replace this with an actual seller ID
+    // Login as a seller to obtain the authorization token
+    const sellerCredentials = {
+      email: 'deleteproduct@example.com',
+      password: 'qwerre'
     };
 
-    const createRes = await chai.request(server)
-      .post('/api/products/createProduct')
-      .set('Authorization', `Bearer ${sellerToken}`)
-      .send(newProduct);
+    const res = await chai.request(server)
+      .post('/api/users/login')
+      .send(sellerCredentials);
 
-    productId = createRes.body._id;
+    sellerToken = res.body.token;
   });
 
-  it('should delete a product by a seller', (done) => {
-    chai.request(server)
-      .delete(`/api/users/${productId}`)
-      .set('Authorization', `Bearer ${sellerToken}`)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.property('message').eql('Product deleted successfully.');
-        done();
-      });
+  describe('Delete Product', () => {
+    let productId;
+
+    before(async () => {
+      // Create a new product
+      const newProduct = {
+        name: 'Test Product',
+        description: 'Test Product Description',
+        price: 10,
+        category: 'Test Category',
+        seller: '643885909545b9fda39f31ec', // Replace this with an actual seller ID
+      };
+
+      const createRes = await chai.request(server)
+        .post('/api/products/createProduct')
+        .set('Authorization', `Bearer ${sellerToken}`)
+        .send(newProduct);
+
+      productId = createRes.body._id;
+    });
+
+    it('should delete a product by a seller', (done) => {
+      chai.request(server)
+        .delete(`/api/users/${productId}`)
+        .set('Authorization', `Bearer ${sellerToken}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Product deleted successfully.');
+          done();
+        });
+    });
   });
 });
