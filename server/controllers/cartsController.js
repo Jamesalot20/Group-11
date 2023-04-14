2835d794471ef8a39c157369f4013452a33ace5b
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-13 23:00:59 -0500

    Update cartsController.js

--- server/controllers/cartsController.js
+++ server/controllers/cartsController.js
@@ -18,29 +18,20 @@ exports.getCartByUser = async (req, res) => {
 
 exports.addItemToCart = async (req, res) => {
   console.log("addItemToCart req.user:", req.user);
-  console.log('addItemToCart called'); // Add this line
-  console.log('Request user:', req.user); // Add this line
+  console.log('addItemToCart called');
+  console.log('Request user:', req.user);
   try {
     const { productId, quantity } = req.body;
-    const cart = await Cart.findOne({ user: req.user.userId });
-   console.log('userId:', req.user._id); // Add this line
-    console.log('Cart:', cart); // Add this line
+    const cart = await Cart.findOne({ user: req.user._id }); // Change this line
+    console.log('userId:', req.user._id);
+    console.log('Cart:', cart);
     if (!cart) {
       return res.status(404).json({ message: 'Cart not found.' });
     }
 
-    const itemIndex = cart.items.findIndex((item) => item.product.toString() === productId);
-
-    if (itemIndex >= 0) {
-      cart.items[itemIndex].quantity += quantity;
-    } else {
-      cart.items.push({ product: productId, quantity });
-    }
-
-    await cart.save();
-    res.status(200).json({ message: 'Item added to cart.', cart });
+    // ... rest of the function code
   } catch (error) {
-     console.log('Error:', error.message);
+    console.log('Error:', error.message);
     res.status(500).json({ message: 'Server error.' });
   }
 };
