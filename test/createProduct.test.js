3dd1f534c2346ed360b1914c78cec9cbf3d57b67
JDMCCOY-dev <85303175+JDMCCOY-dev@users.noreply.github.com>
2023-04-13 21:50:29 -0500

    Create createProduct.test.js

---
+++ test/createProduct.test.js
@@ -0,0 +1,85 @@
+const request = require('supertest');
+const app = require('../server');
+
+describe('POST /api/users/createProduct', () => {
+  let sellerToken;
+
+  beforeAll((done) => {
+    // Log in as a seller and get the access token
+    request(app)
+      .post('/api/users/login')
+      .send({ email: 'seller@example.com', password: 'password' })
+      .end((err, res) => {
+        if (err) return done(err);
+        sellerToken = res.body.token;
+        done();
+      });
+  });
+
+  it('should create a new product', (done) => {
+    const newProduct = {
+      name: 'Test Product',
+      description: 'This is a test product',
+      price: 9.99,
+      quantity: 10,
+      seller: 'seller@example.com',
+    };
+    request(app)
+      .post('/api/users/createProduct')
+      .send(newProduct)
+      .set('Authorization', `Bearer ${sellerToken}`)
+      .expect(201)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.message).toEqual('Product successfully created.');
+        done();
+      });
+  });
+
+  it('should not create a new product if user is not authenticated', (done) => {
+    const newProduct = {
+      name: 'Test Product',
+      description: 'This is a test product',
+      price: 9.99,
+      quantity: 10,
+      seller: 'seller@example.com',
+    };
+    request(app)
+      .post('/api/users/createProduct')
+      .send(newProduct)
+      .expect(401)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.message).toEqual('Authentication required.');
+        done();
+      });
+  });
+
+  it('should not create a new product if user is not a seller', (done) => {
+    const newProduct = {
+      name: 'Test Product',
+      description: 'This is a test product',
+      price: 9.99,
+      quantity: 10,
+      seller: 'seller@example.com',
+    };
+    // Log in as a buyer and get the access token
+    request(app)
+      .post('/api/users/login')
+      .send({ email: 'buyer@example.com', password: 'password' })
+      .end((err, res) => {
+        if (err) return done(err);
+        const buyerToken = res.body.token;
+        request(app)
+          .post('/api/users/createProduct')
+          .send(newProduct)
+          .set('Authorization', `Bearer ${buyerToken}`)
+          .expect(403)
+          .end((err, res) => {
+            if (err) return done(err);
+            expect(res.body.message).toEqual('You are not authorized to perform this action.');
+            done();
+          });
+      });
+  });
+});
