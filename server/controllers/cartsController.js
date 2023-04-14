9ede4a0187d5e93721ef66b848d3f6ddebf341be
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-13 23:05:48 -0500

    Update cartsController.js

--- server/controllers/cartsController.js
+++ server/controllers/cartsController.js
@@ -22,7 +22,7 @@ exports.addItemToCart = async (req, res) => {
   console.log('Request user:', req.user);
   try {
     const { productId, quantity } = req.body;
-    onst cart = await Cart.findOne({ user: req.user.userId }); // Change this line
+    const cart = await Cart.findOne({ user: req.user.userId }); // Change this line
     console.log('userId:', req.user.userId)
     console.log('Cart:', cart);
     if (!cart) {
