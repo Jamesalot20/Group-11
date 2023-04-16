df231c588a29a542ae8ffd0b67d036dbed4c443a
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-15 20:08:34 -0500

    Create Adminban.test.js

---
+++ test/Adminban.test.js
@@ -0,0 +1,56 @@
+const request = require('supertest');
+const app = require('../app'); // Import your Express app
+const User = require('../models/User');
+
+describe('Admin banning a user', () => {
+  let adminToken;
+  let userEmail;
+
+  beforeAll(async () => {
+    // Create an admin user and get its token
+    const adminUser = new User({
+      email: 'admin@example.com',
+      password: 'AdminPassword123',
+      role: 'admin',
+    });
+
+    await adminUser.save();
+
+    const response = await request(app)
+      .post('/login')
+      .send({
+        email: 'admin@example.com',
+        password: 'AdminPassword123',
+      });
+
+    adminToken = response.body.token;
+
+    // Create a user to be banned
+    const userToBeBanned = new User({
+      email: 'user@example.com',
+      password: 'UserPassword123',
+      role: 'buyer',
+    });
+
+    await userToBeBanned.save();
+    userEmail = userToBeBanned.email;
+  });
+
+  afterAll(async () => {
+    // Clean up the test users
+    await User.deleteOne({ email: 'admin@example.com' });
+    await User.deleteOne({ email: 'user@example.com' });
+  });
+
+  test('Admin can ban a user', async () => {
+    const response = await request(app)
+      .delete(`/deleteUser/${userEmail}`)
+      .set('Authorization', `Bearer ${adminToken}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('User account deleted successfully.');
+
+    const deletedUser = await User.findOne({ email: userEmail });
+    expect(deletedUser).toBeNull();
+  });
+});
