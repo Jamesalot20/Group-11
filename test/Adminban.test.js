89568d62d5d591c640b28d119be72b338c057e28
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-16 17:56:17 -0500

    Update Adminban.test.js

--- test/Adminban.test.js
+++ test/Adminban.test.js
@@ -1,11 +1,14 @@
 const request = require('supertest');
-const app = require('../server/server');
+const chai = require('chai');
+const server = require('../server/server');
+
+const { expect } = chai;
 
 describe('Admin banning a user', () => {
   let adminToken;
   let userEmail;
 
-  beforeAll(async () => {
+  before(async () => {
     // Create an admin user and get its token
     const adminUser = new User({
       email: 'admin@example.com',
@@ -35,21 +38,21 @@ describe('Admin banning a user', () => {
     userEmail = userToBeBanned.email;
   });
 
-  afterAll(async () => {
+  after(async () => {
     // Clean up the test users
     await User.deleteOne({ email: 'admin@example.com' });
     await User.deleteOne({ email: 'user@example.com' });
   });
 
-  test('Admin can ban a user', async () => {
+  it('Admin can ban a user', async () => {
     const response = await request(app)
       .delete(`/deleteUser/${userEmail}`)
       .set('Authorization', `Bearer ${adminToken}`);
 
-    expect(response.status).toBe(200);
-    expect(response.body.message).toBe('User account deleted successfully.');
+    expect(response.status).to.equal(200);
+    expect(response.body.message).to.equal('User account deleted successfully.');
 
     const deletedUser = await User.findOne({ email: userEmail });
-    expect(deletedUser).toBeNull();
+    expect(deletedUser).to.be.null;
   });
 });
