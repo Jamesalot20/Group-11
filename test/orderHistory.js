03c1686156bc60e2f7bab53a66616f219239a622
JDMCCOY-dev <85303175+JDMCCOY-dev@users.noreply.github.com>
2023-04-16 20:35:38 -0500

    Update orderHistory.js

--- test/orderHistory.js
+++ test/orderHistory.js
@@ -1,24 +1,28 @@
-/*
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../server');
-const expect = chai.expect;
+const app = require('../server/server');
 
 chai.use(chaiHttp);
+chai.should();
 
-describe('Order history', () => {
-  it('should return the order history for a user', (done) => {
-    chai.request(app)
-      .get('/api/orders/history/12345') // Replace with a valid user ID
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an('array');
-        expect(res.body[0]).to.have.property('orderId');
-        expect(res.body[0]).to.have.property('items');
-        expect(res.body[0].items[0]).to.have.property('productId');
-        expect(res.body[0].items[0]).to.have.property('quantity');
-        done();
-      });
+describe('Order history API', () => {
+  let token;
+
+  before(async () => {
+    // Log in as a buyer to obtain an access token
+    const res = await chai.request(app)
+      .post('/api/users/login')
+      .send({ email: 'buyer@test.com', password: 'password' });
+
+    token = res.body.token;
+  });
+
+  it('should return a buyer\'s order history', async () => {
+    const res = await chai.request(app)
+      .get('/api/orders/history')
+      .set('Authorization', `Bearer ${token}`);
+
+    res.should.have.status(200);
+    res.body.should.be.an('array');
   });
 });
-*/
