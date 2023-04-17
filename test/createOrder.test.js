8e3bd8926a406b7b414f1fecb3efa8f36fae3224
JDMCCOY-dev <85303175+JDMCCOY-dev@users.noreply.github.com>
2023-04-16 21:56:04 -0500

    Update createOrder.test.js

--- test/createOrder.test.js
+++ test/createOrder.test.js
@@ -18,52 +18,54 @@ describe('Order history API', () => {
     token = res.body.token;
   });
 
-  describe('POST /api/orders', () => {
+describe('Order history API', () => {
+  let token;
+
+  before(async () => {
+    // Log in as a buyer to obtain an access token
+    const res = await chai.request(app)
+      .post('/api/users/login')
+      .send({ email: 'test@example.com', password: 'password123' });
+
+    token = res.body.token;
+  });
+
+  describe('Create order', () => {
     it('should create a new order', async () => {
-      // Create a new order
+      const order = {
+        "buyer": "64384ac5734ffb9222e8a989",
+        "items": [
+          {
+            "product": "64387bd7c54a9c45b738eba3",
+            "quantity": 2,
+            "price": 599.98
+          },
+          {
+            "product": "6438d521ea7f3e39c5ec31b9",
+            "quantity": 1,
+            "price": 9.99
+          }
+        ],
+        "totalPrice": 27.97,
+        "status": "pending",
+        "createdAt": "2022-04-13T10:00:00.000Z",
+        "updatedAt": "2022-04-13T10:00:00.000Z"
+      };
+      
       const res = await chai.request(app)
         .post('/api/orders')
         .set('Authorization', `Bearer ${token}`)
-        .send({
-          items: [
-            {
-              productId: 'product-id-1',
-              quantity: 2,
-            },
-            {
-              productId: 'product-id-2',
-              quantity: 3,
-            },
-          ],
-          shippingAddress: {
-            address: '123 Main St',
-            city: 'Anytown',
-            state: 'CA',
-            zipCode: '12345',
-          },
-        });
-
-      // Verify the response
+        .send(order);
+      
       res.should.have.status(201);
-      res.body.should.be.an('object');
-      res.body.should.have.property('id');
-      res.body.should.have.property('items');
-      res.body.should.have.property('shippingAddress');
-      res.body.items.should.be.an('array').with.lengthOf(2);
-      res.body.items[0].should.be.an('object').with.property('productId', 'product-id-1');
-      res.body.items[0].should.be.an('object').with.property('quantity', 2);
-      res.body.items[1].should.be.an('object').with.property('productId', 'product-id-2');
-      res.body.items[1].should.be.an('object').with.property('quantity', 3);
-      res.body.shippingAddress.should.be.an('object');
-      res.body.shippingAddress.should.have.property('address', '123 Main St');
-      res.body.shippingAddress.should.have.property('city', 'Anytown');
-      res.body.shippingAddress.should.have.property('state', 'CA');
-      res.body.shippingAddress.should.have.property('zipCode', '12345');
-
-      // Store the order for later use
-      order = res.body;
+      res.body.should.be.a('object');
+      res.body.should.have.property('buyer').eql(order.buyer);
+      res.body.should.have.property('items').deep.eql(order.items);
+      res.body.should.have.property('totalPrice').eql(order.totalPrice);
+      res.body.should.have.property('status').eql(order.status);
+      res.body.should.have.property('createdAt');
+      res.body.should.have.property('updatedAt');
     });
   });
-
-  // Add more test cases here, such as GET /api/orders to retrieve the created order
 });
+
