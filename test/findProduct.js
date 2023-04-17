e5d94e8ac9c5accb672ec6787146048cd0bf49c5
JDMCCOY-dev <85303175+JDMCCOY-dev@users.noreply.github.com>
2023-04-16 23:42:03 -0500

    Update findProduct.js

--- test/findProduct.js
+++ test/findProduct.js
@@ -2,41 +2,61 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../server/server');
 
-// Configure chai
 chai.use(chaiHttp);
 chai.should();
 
-describe("Product Routes", () => {
+describe('Product search by ID', () => {
+  let authToken;
 
-  describe("GET /api/products/:productId", () => {
-    it("should return the product with the given ID", (done) => {
-      const productId = "643ccc56a2578e3450a14d2e";
-
-      chai.request(app)
-        .get(`/api/products/${productId}`)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          res.body.should.have.property('_id');
-          res.body.should.have.property('name');
-          res.body.should.have.property('description');
-          res.body.should.have.property('price');
-          res.body.should.have.property('quantity');
-          res.body.should.have.property('seller');
-          done();
-        });
-    });
-
-    it("should return 404 if the product ID is invalid", (done) => {
-      const productId = "0123456789";
-
-      chai.request(app)
-        .get(`/api/products/${productId}`)
-        .end((err, res) => {
-          res.should.have.status(404);
-          done();
-        });
-    });
+  // Get auth token before running tests
+  before((done) => {
+    chai.request(app)
+      .post('/api/users/login')
+      .send({ email: 'buyer@example.com', password: 'password123' })
+      .end((err, res) => {
+        authToken = res.body.token;
+        done();
+      });
   });
 
+  it('should return a product by ID', (done) => {
+    const productId = '643ccd3aebe48b07e6c7f157';
+    chai.request(app)
+      .get(`/api/products/${productId}`)
+      .set('Authorization', `Bearer ${authToken}`)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('name');
+        res.body.should.have.property('description');
+        res.body.should.have.property('price');
+        done();
+      });
+  });
+
+  it('should return an error when the product ID is not found', (done) => {
+    const productId = '32543';
+    chai.request(app)
+      .get(`/api/products/${productId}`)
+      .set('Authorization', `Bearer ${authToken}`)
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.body.should.be.a('object');
+        res.body.should.have.property('message').eql('Product not found.');
+        done();
+      });
+  });
+
+  it('should return an error when an invalid product ID is provided', (done) => {
+    const productId = 'invalidid';
+    chai.request(app)
+      .get(`/api/products/${productId}`)
+      .set('Authorization', `Bearer ${authToken}`)
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.a('object');
+        res.body.should.have.property('message').eql('Invalid product ID.');
+        done();
+      });
+  });
 });
