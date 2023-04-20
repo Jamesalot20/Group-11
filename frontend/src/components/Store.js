92d335e9c640653e4d066e8b7001c4c04087fbe8
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-20 17:59:13 -0500

    Update Store.js

--- frontend/src/components/Store.js
+++ frontend/src/components/Store.js
@@ -1,31 +1,59 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
+import api from '../api';
 
-import { AiFillCloseCircle } from "react-icons/ai";
-import { RiDeleteBin6Line } from "react-icons/ri";
+const Store = () => {
+  const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState('');
+  const [category, setCategory] = useState('');
 
+  useEffect(() => {
+    getProducts();
+  }, [category]);
 
-
-export default class Store extends Component {
-    render() {
-        return (
-            <form>
-
-                <h1>TechTonic</h1>
-                <div className="input-wrapper">
-                    <input placeholder="Type to search products" />
-                </div>
-                <div className="Products">
-
-                </div>
-                <div className="cart">
-                    <h2>ShoppingCart</h2>
-                </div>
-
-
-
-
-
-            </form>
-        )
+  const getProducts = async () => {
+    try {
+      const response = await api.get('/api/products/getProducts', {
+        params: { search, category },
+      });
+      setProducts(response.data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
     }
-}
+  };
+
+  return (
+    <div>
+      <h1>Store</h1>
+      <input
+        type="text"
+        placeholder="Search for products"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={getProducts}>Search</button>
+
+      <select
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value="">All Categories</option>
+        <option value="category1">Category 1</option>
+        <option value="category2">Category 2</option>
+        <option value="category3">Category 3</option>
+        {/* Add more categories here */}
+      </select>
+
+      <div>
+        {products.map((product) => (
+          <div key={product._id}>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>Price: ${product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Store;
