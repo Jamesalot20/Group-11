b7c176a7e79e6e0a7dce7679d55d9b132e8fa2a5
JDMCCOY-dev <85303175+JDMCCOY-dev@users.noreply.github.com>
2023-04-20 22:38:00 -0500

    Update Checkout.js

--- frontend/src/components/Checkout.js
+++ frontend/src/components/Checkout.js
@@ -1,72 +1,66 @@
-import React, { Component } from 'react'
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export default class Checkout extends Component {
-    render() {
-        
-        const handleSubmit = () => {
-            const navigate = useNavigate();
-            // Redirect to the Login page
-            navigate('/Store');
+function Checkout() {
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    // Redirect to the Login page
+    navigate('/Store');
   };
-        
-        return (
-            <form>
 
-                <h1>Checkout</h1>
-                <h2>Total:</h2>
+  return (
+    <form>
+      <h1>Checkout</h1>
+      <h2>Total:</h2>
 
-                <div className="Products">
+      <div className="Products"></div>
 
-                </div>
-
-                <div className="mb-3">
-                    <label>Address</label>
-                    <input
-                        type="address"
-                        className="form-control"
-                        placeholder="Enter address"
-                    />
-                </div>
-                <div className="mb-3">
-                    <label>Card Number</label>
-                    <input
-                        type="card number"
-                        className="form-control"
-                        placeholder="Enter card number"
-                    />
-                </div>
-                <div className="mb-3">
-                    <label>City</label>
-                    <input
-                        type="city"
-                        className="form-control"
-                        placeholder="Enter city"
-                    />
-                </div>
-                <div className="mb-3">
-                    <label>State</label>
-                    <input
-                        type="state"
-                        className="form-control"
-                        placeholder="Enter state"
-                    />
-                </div>
-                <div className="d-grid">
-                    <a href="Completion">
-                        <button ontype="submit" className="btn btn-primary" onClick={handleSubmit}>
-                            Submit
-                        </button>
-                    </a>
-                </div>
-
-
-
-
-
-
-
-            </form>
-        )
-    }
+      <div className="mb-3">
+        <label>Address</label>
+        <input
+          type="address"
+          className="form-control"
+          placeholder="Enter address"
+        />
+      </div>
+      <div className="mb-3">
+        <label>Card Number</label>
+        <input
+          type="card number"
+          className="form-control"
+          placeholder="Enter card number"
+        />
+      </div>
+      <div className="mb-3">
+        <label>City</label>
+        <input
+          type="city"
+          className="form-control"
+          placeholder="Enter city"
+        />
+      </div>
+      <div className="mb-3">
+        <label>State</label>
+        <input
+          type="state"
+          className="form-control"
+          placeholder="Enter state"
+        />
+      </div>
+      <div className="d-grid">
+        <a href="Completion">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </a>
+      </div>
+    </form>
+  );
 }
+
+export default Checkout;
