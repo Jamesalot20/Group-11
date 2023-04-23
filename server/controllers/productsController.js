a004138bc23aa0a963a614fab44fe4110f4a296e
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-23 18:37:22 -0500

    Update productsController.js

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -3,9 +3,22 @@ const mongoose = require('mongoose');
 
 exports.getProducts = async (req, res) => {
   try {
-    const products = await Product.find();
+    const { search, category } = req.query;
+
+    let query = {};
+
+    if (search) {
+      query.name = { $regex: search, $options: 'i' };
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    const products = await Product.find(query);
     res.status(200).json(products);
   } catch (error) {
+    console.error('Error fetching products:', error);
     res.status(500).json({ error: 'An error occurred while fetching products.' });
   }
 };
