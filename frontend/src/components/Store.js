4a6d9b0af41e54110e17cb84181377929442dce6
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-23 17:30:09 -0500

    Update Store.js

--- frontend/src/components/Store.js
+++ frontend/src/components/Store.js
@@ -40,9 +40,9 @@ const Store = () => {
         onChange={(e) => setCategory(e.target.value)}
       >
         <option value="">All Categories</option>
-        <option value="category1">Category 1</option>
-        <option value="category2">Category 2</option>
-        <option value="category3">Category 3</option>
+        <option value="category1">GPU</option>
+        <option value="category2">CPU</option>
+        <option value="category3">RAM</option>
         {/* Add more categories here */}
       </select>
 
