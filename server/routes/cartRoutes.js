41318fa5463ffb5934d17a9d74dd471eafc39286
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-23 11:49:47 -0500

    Update cartRoutes.js

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -4,7 +4,7 @@ const cartsController = require('../controllers/cartsController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/', authMiddleware.authenticate, cartsController.getCartByUser);
-router.post('/add',cartsController.addItemToCart);
+router.post('/add', authMiddleware.authenticate, cartsController.addItemToCart);
 router.put('/update', authMiddleware.authenticate, cartsController.updateCartItem);
 router.delete('/remove/:productId', authMiddleware.authenticate, cartsController.removeCartItem);
 
