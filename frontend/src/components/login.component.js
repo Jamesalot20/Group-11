7e215f3137a8b8ba2a4edbdb1519b9e3b9699fe6
JDMCCOY-dev <85303175+JDMCCOY-dev@users.noreply.github.com>
2023-04-24 00:22:08 -0500

    Update login.component.js

--- frontend/src/components/login.component.js
+++ frontend/src/components/login.component.js
@@ -8,22 +8,26 @@ const Login = () => {
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    try {
-      const response = await api.post('/users/login', { email, password });
-      const { token } = response.data;
+  try {
+    const response = await api.post('/users/login', { email, password });
+    const { token, role } = response.data;
 
-      // Save the token in local storage or any other preferred storage
-      localStorage.setItem('authToken', token);
+    // Save the token in local storage or any other preferred storage
+    localStorage.setItem('authToken', token);
 
-      // Redirect to the Store page
+    // Redirect to the appropriate page based on the user's role
+    if (role === 'admin') {
+      navigate('/admin');
+    } else {
       navigate('/store');
-    } catch (error) {
-      console.error('Error logging in:', error);
-      // Show an error message or handle the error as needed
     }
-  };
+  } catch (error) {
+    console.error('Error logging in:', error);
+    // Show an error message or handle the error as needed
+  }
+};
 
   return (
     <form onSubmit={handleSubmit}>
