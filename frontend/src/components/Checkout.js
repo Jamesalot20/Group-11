e112c00fc23b0f02d9dc7a3c3a416646a510d8d2
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-24 17:40:10 -0500

    Update Checkout.js

--- frontend/src/components/Checkout.js
+++ frontend/src/components/Checkout.js
@@ -48,6 +48,7 @@ const handleSubmit = async (e) => {
 async function createOrder(orderData) {
   try {
     console.log('Token:', localStorage.getItem('token'));
+    
     const response = await fetch('http://localhost:5000/api/orders', {
       method: 'POST',
       headers: {
