23ada18033e00f78b06c78c86730fa0a019f897c
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-23 21:02:36 -0500

    Update CartContext.js

--- frontend/src/components/CartContext.js
+++ frontend/src/components/CartContext.js
@@ -7,14 +7,14 @@ export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
   const [productDetails, setProductDetails] = useState({});
 
-  const fetchProductDetails = async (product) => {
+  const fetchProductDetails = async (productId) => {
     
-    if (!product) return;
+    if (!productId) return;
     try {
-      const response = await api.get(`/products/${product}`);
+      const response = await api.get(`/products/${productId}`);
       setProductDetails((prevDetails) => ({
         ...prevDetails,
-        [product]: response.data,
+        [productId]: response.data,
       }));
     } catch (error) {
       console.error('Error fetching product details:', error);
@@ -24,14 +24,14 @@ console.log('productDetails:', productDetails);
 
   };
 
-  const addToCart = async (product) => {
+  const addToCart = async (productId) => {
     try {
       // Retrieve the authentication token from local storage or another source
       const token = localStorage.getItem('authToken');
 
       const response = await api.post(
         '/carts/add',
-        { product, quantity: 1 },
+        { productId, quantity: 1 },
         {
           headers: {
             'Authorization': `Bearer ${token}`,
@@ -39,7 +39,7 @@ console.log('productDetails:', productDetails);
         }
       );
       setCartItems(response.data.cart.items);
-      fetchProductDetails(product);
+      fetchProductDetails(productId);
     } catch (error) {
       console.error('Error adding item to cart:', error);
     }
