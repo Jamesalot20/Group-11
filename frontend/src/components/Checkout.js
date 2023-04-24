47b9e74913718c197d48f484d89ed2f499f08d02
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-24 00:06:23 -0500

    Update Checkout.js

--- frontend/src/components/Checkout.js
+++ frontend/src/components/Checkout.js
@@ -11,8 +11,31 @@ function Checkout() {
     return total + (product ? product.price * item.quantity : 0);
   }, 0);
 
-  const handleSubmit = () => {
-    navigate('/Store');
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    const orderItems = cartItems.map((item) => ({
+      product: item.product,
+      quantity: item.quantity,
+    }));
+
+    const response = await fetch('/api/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      body: JSON.stringify({
+        items: orderItems,
+        totalPrice: totalPrice,
+      }),
+    });
+
+    if (response.ok) {
+      navigate('/Completion');
+    } else {
+      console.error('Failed to submit order:', response.status);
+    }
   };
 
   return (
