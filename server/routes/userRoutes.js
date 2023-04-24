184d5a8a31a37702137bec1adbc18fa3d27dd4de
William-Emmich <117790219+William-Emmich@users.noreply.github.com>
2023-04-23 23:10:51 -0500

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -12,8 +12,7 @@ router.post('/logout', authMiddleware.authenticate, usersController.logoutUser);
 router.delete('/deleteUser/:email', authMiddleware.authenticate, authMiddleware.authorize(['admin']), usersController.deleteUser);
 router.delete('/:productId', authMiddleware.authenticate, authMiddleware.authorize(['seller', 'admin']), productsController.deleteProduct);
 
-router.get('/pendingUsers', authMiddleware.authenticate, authMiddleware.authorize(['admin']), usersController.getPendingUsers);
-router.put('/updateUserApprovalStatus/:email', authMiddleware.authenticate, authMiddleware.authorize(['admin']), usersController.updateUserApprovalStatus);
+
 
 
 
