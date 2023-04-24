24001a243e34cbdb167d03762eccbb1ac44ff65f
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-24 16:15:50 -0500

    Update login.component.js

--- frontend/src/components/login.component.js
+++ frontend/src/components/login.component.js
@@ -12,13 +12,14 @@ const Login = () => {
 
   try {
     const response = await api.post('/users/login', { email, password });
-    const { token, role } = response.data;
+    const { token, userId, role } = response.data;
 
-    // Save the token in local storage or any other preferred storage
+    // Save the token and user ID in local storage or any other preferred storage
     localStorage.setItem('authToken', token);
+    localStorage.setItem('userId', userId);
 
     // Redirect to the appropriate page based on the user's role
-    if (role === 'admin' || 'Admin') {
+    if (role === 'admin' || role === 'Admin') {
       navigate('/Admin');
     } else {
       navigate('/Store');
