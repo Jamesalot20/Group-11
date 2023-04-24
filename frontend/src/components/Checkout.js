a3b3071d8169419e4cfa5ac013a4705b332d5508
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-24 12:41:10 -0500

    Update Checkout.js

--- frontend/src/components/Checkout.js
+++ frontend/src/components/Checkout.js
@@ -75,7 +75,7 @@ async function createOrder(orderData) {
 
       <div className="Products">
         {cartItems.map((item, index) => {
-          const product = productDetails[item.product];
+          const product = productDetails[item.productId];
           return product ? (
             <div key={index}>
               <h3>{product.name}</h3>
