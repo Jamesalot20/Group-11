0d54ab3ff1d05618794f6ace18bd6f3ed619ec11
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-24 19:42:01 -0500

    Update Orders.js

--- frontend/src/components/Orders.js
+++ frontend/src/components/Orders.js
@@ -3,46 +3,58 @@ import api from '../api';
 
 function OrdersPage() {
   const [orders, setOrders] = useState([]);
-  const [returningProductId, setReturningProductId] = useState(null);
+  const [returningItemId, setReturningItemId] = useState(null);
 
   // Fetch orders on mount
-useEffect(() => {
-  async function fetchOrders() {
+  useEffect(() => {
+    async function fetchOrders() {
+      try {
+        const response = await api.get('/orders/history', {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+          },
+        });
+        console.log('Fetched orders:', response.data);
+        setOrders(response.data);
+      } catch (error) {
+        console.error('Error fetching orders:', error);
+      }
+    }
+    fetchOrders();
+  }, []);
+
+  async function handleReturn(itemId) {
     try {
-      const response = await api.get('/orders/history', {
+      const response = await api.put(`/orders/item/${itemId}/return`, {}, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('authToken')}`,
         },
       });
-      console.log('Fetched orders:', response.data);
-      setOrders(response.data);
+
+      if (response.status === 200) {
+        setReturningItemId(itemId);
+
+        setOrders((prevOrders) =>
+          prevOrders.map((order) => {
+            const items = order.items.map((item) => {
+              if (item._id === itemId) {
+                return { ...item, status: "returned" };
+              } else {
+                return item;
+              }
+            });
+            return { ...order, items };
+          })
+        );
+      } else {
+        console.error('Failed to return item:', response.status);
+      }
     } catch (error) {
-      console.error('Error fetching orders:', error);
+      console.error('Error returning item:', error);
     }
   }
-  fetchOrders();
-}, []);
 
-
-async function handleReturn(itemId) {
-  try {
-    const response = await api.put(`/orders/item/${itemId}/return`, {}, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
-      },
-    });
-
-    if (response.status === 200) {
-      setReturningItemId(itemId);
-    } else {
-      console.error('Failed to return item:', response.status);
-    }
-  } catch (error) {
-    console.error('Error returning item:', error);
-  }
-}
-
-   return (
+  return (
     <div>
       <h1>Orders</h1>
       {orders.length === 0 ? (
@@ -81,7 +93,7 @@ async function handleReturn(itemId) {
           </div>
         ))
       )}
-      {returningProductId && <p>Product returned.</p>}
+      {returningItemId && <p>Product with ID {returningItemId} returned.</p>}
     </div>
   );
 }
