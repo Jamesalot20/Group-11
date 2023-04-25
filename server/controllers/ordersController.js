cded1e7723eeca02b0d6c3d2b5193b9556620814
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-04-24 19:35:54 -0500

    Update ordersController.js

--- server/controllers/ordersController.js
+++ server/controllers/ordersController.js
@@ -11,6 +11,31 @@ exports.getOrdersByUser = async (req, res) => {
   }
 };
 
+exports.returnOrderItem = async (req, res) => {
+  try {
+    const itemId = req.params.itemId;
+    const userId = req.user.userId;
+
+    const orderItem = await OrderItem.findOne({ _id: itemId, buyer: userId });
+
+    if (!orderItem) {
+      return res.status(404).json({ message: 'Order item not found.' });
+    }
+
+    if (orderItem.status !== 'delivered') {
+      return res.status(400).json({ message: 'Cannot return an item that is not delivered.' });
+    }
+
+    orderItem.status = 'returned';
+    await orderItem.save();
+
+    res.status(200).json({ message: 'Order item returned successfully.' });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while returning the order item.' });
+  }
+};
+
+
 exports.getOrderById = async (req, res) => {
   try {
     const order = await Order.findById(req.params.id);
