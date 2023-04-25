f3d6acac5e9da7791db03008de51e652215df63a
JDMCCOY-dev <85303175+JDMCCOY-dev@users.noreply.github.com>
2023-04-24 22:28:54 -0500

    Update usersController.js

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -95,4 +95,12 @@ exports.deleteUser = async (req, res) => {
   }
 };
 
-
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Error in getAllUsers:', error);
+    res.status(500).json({ message: 'Server error.' });
+  }
+};
