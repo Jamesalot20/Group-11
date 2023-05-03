9c7d0f25a0df183334fe409187f1d63949392542
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2023-05-02 21:45:04 -0500

    Update AuthenticatedNavigation.js

--- frontend/src/components/AuthenticatedNavigation.js
+++ frontend/src/components/AuthenticatedNavigation.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const AuthenticatedNavigation = () => {
+const AuthenticatedNavigation = ({ role }) => {
   return (
     <nav>
       <NavLink to="/store">Store</NavLink>
-      <NavLink to="/my-products">My Products</NavLink>
+      {['seller', 'admin'].includes(role) && <NavLink to="/my-products">My Products</NavLink>}
       {/* Add more navigation links as needed */}
     </nav>
   );
